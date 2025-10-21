ef8a7e9ad45e0a4c6b9a7673dfc0268a7ed10ad9
Gia Long <gialongchuai@gmail.com>
2025-10-21 08:23:31 +0700

    js

--- js/lesson_1/main5.js
+++ js/lesson_1/main5.js
@@ -22,4 +22,17 @@ function writeArgu() {
     console.log(arguments);
 };
 
-writeArgu('hello world!', 'hello universe'); // Arguments(2) ['hello world!', 'hello universe', callee: ƒ, Symbol(Symbol.iterator): ƒ]
+writeArgu('hello world!', 'hello universe'); // Arguments(2) ['hello world!', 'hello universe'ẽi callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+console.log(1,2,3,4,5); // 1 2 3 4 5
+
+function run() {
+    var myO = '';
+    for(var par of arguments) {
+        console.log(par);
+        myO += `${par} - `;
+    }
+    console.log(myO); // xin chao - hello world - 
+}
+
+run('xin chao', 'hello world'); // 2 dòng: xin chao : hello world
