b68af103e3d86550e48cd562d747136994e0e201
Gia Long <gialongchuai@gmail.com>
2025-10-28 15:27:16 +0700

    js validate nhieu rule cho cac input

--- js/lesson_10/main.js
+++ js/lesson_10/main.js
@@ -1,29 +1,50 @@
-
-
-function validate(inputEle, rule, message) {
-    var errorMessage = rule.test(inputEle.value);
-    if(errorMessage) { 
-        message.innerText = errorMessage;
-        console.log(message.parentElement); 
-        message.parentElement.classList.add('invalid');
-    } else { 
-        message.innerText = '';
-        console.log(message.parentElement);
-        message.parentElement.classList.remove('invalid');
-    }
-}
-
 function Validator(options) {
+    var optionRules = {};
+    
+    function validate(inputEle, rule, message) {
+        //var errorMessage = rule.test(inputEle.value);
+        var errorMessage;
+        
+        var arrayRules = optionRules[rule.option];
+        for(var i=0; i<arrayRules.length; i++) {
+            errorMessage = arrayRules[i](inputEle.value); // hàm khá lạ so với dòng số 5
+            if(errorMessage) break; // nếu bắt gặp lỗi đầu tiên thì break luôn khỏi xét lỗi thứ 2
+        }
+
+        if(errorMessage) { 
+            message.innerText = errorMessage;
+            // console.log(message.parentElement); 
+            message.parentElement.classList.add('invalid');
+        } else { 
+            message.innerText = '';
+            console.log(message.parentElement);
+            message.parentElement.classList.remove('invalid');
+        }
+    }
+
     console.log(options); // {form: '#form-1', rules: Array(2)}
     var formElement = document.querySelector(options.form); // bên kia gán là form-1
     if(formElement) { // nếu có giá trị tức là tìm thấy form-1
         options.rules.forEach(function(rule) { // đi qua phần tử thứ 2, do form là phần tử 1 đi rồi, duyệt các rule 
+            
+            // Lưu lại các rules cho mỗi input
+            // optionRules[rule.option] = rule.test; // here : gán cho optin tức
+            // ví dụ : option : #fullName = cái hàm test(); coi code cuối cùng trang
+            console.log(rule.option);
+
+            if(Array.isArray(optionRules[rule.option])) { // khi mà chưa ó gì thì nó là undefined
+                optionRules[rule.option].push(rule.test); // đã từng tồn tại phần tử rồi nên chỉ cần push vào
+            } else {
+                optionRules[rule.option] = [rule.test]; // do lần nó là 1 object rỗng nên
+            } // nó là undefined và ta tạo với option đó là 1 cái mảng để lần sau có thể push thêm rule vào
+
+            // với mỗi rule nó là 1 cái hàm bên dưới cùng code gồm có option và test (above)
             var inputEle = document.querySelector(rule.option); // VÍ DỤ 1 ÔNG LÀ #fullname VÀ #email THÌ ĐI QUA TỪNG RULE ĐỂ LẤY CÁI ELE ĐÓ RA
             console.log(inputEle);
             //<input id="fullname" name="fullname" type="text" placeholder="VD: Sơn Đặng" class="form-control"></input>
             //<input id="email" name="email" type="text" placeholder="VD: email@domain.com" class="form-control">
             
-            // **** hoặc có thể cho nó 1 key value bên html: selector : 'form-message' và khi dùng tới chỉ cần chấm giống dòng rule.option
+            // **** hoặc có thể cho nó 1 key value bên html: selector : 'form-message' và khi dùng tới chỉ cần chấm giống dòng rule.form
             var message = inputEle.parentElement.querySelector('.form-message'); // có được ele của 2 input truy ra thằng cha là thẻ div to ở ngoài, xoNG TỪ THẺ DIV ĐÓ TRUY VÀO THẰNG MESSAGE
             console.log(message); // <span class="form-message">Vui lòng nhập trường này!</span> : những thẻ span được viết sẵn để xuống dưới chèn message vào
 
@@ -58,6 +79,7 @@ function Validator(options) {
                 }
             }
         });
+        console.log(optionRules);
     }
 }
 
