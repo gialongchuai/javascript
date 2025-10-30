eece6ee9e1a19a156df5187ef08323edbe09cad2
Gia Long <gialongchuai@gmail.com>
2025-10-30 14:54:36 +0700

    js

--- js/lesson_10/main.js
+++ js/lesson_10/main.js
@@ -26,8 +26,8 @@ function Validator(formSelector) {
                 return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} kí tự!`;
             }
         },
-        max: function(max) {
-            return function(value) {
+        max: function(max) { // max là số 8
+            return function(value) { // value nhập vào
                 return value.length <= max ? undefined : `Vui lòng nhập ít nhất ${max} kí tự!`;
             }
         }
@@ -48,16 +48,42 @@ function Validator(formSelector) {
             // formRules[input.name] = input.getAttribute('rules');
             var rules = input.getAttribute('rules').split('|');
             for(var rule of rules) {
+                // console.log(rule); // required email required min:6
+
+                var opRule = rule.includes(':');
+                if(opRule) { // kiểm tra nếu các rule tồn tại thêm giá trị ví dụ thay vì required thì min:8
+                    var ruleValues = rule.split(':');
+                    rule = ruleValues[0]; // gán lại ví dụ mảng gồm 2 phần từ [0]:min [1]:8
+                }
+
+                var valueValidatorRules = validatorRules[rule];
+                // console.log(valueValidatorRules);
+
+                if(opRule) { // nếu tồn tại trường hợp rule có điều kiện ví dụ min:8
+                    // thay vì chỉ bằng 1 func nhưng ta đã config bên trong nó còn thêm 1 hàm nữa
+                    // var valueValidatorRules = validatorRules[rule];
+
+                    // nhưng đối với trường hợp này value nó là 1 func return 1 funce nên ta truyền số 8 vào
+                    valueValidatorRules = valueValidatorRules(ruleValues[1]);
+                }
+
                 if(Array.isArray(formRules[input.name])) {
-                    formRules[input.name].push(validatorRules[rule]);
+                    formRules[input.name].push(valueValidatorRules);
                 } else {
-                    // formRules[input.name] = validatorRules[rule]; // viết như này là sai nhé, 
+                    // formRules[input.name] = valueValidatorRules; // viết như này là sai nhé, 
                     // đây là gán 1 phần tử trong mảng chỉ chứa 1 phần tử
                     // chứ không phải là gán phần tử đó chứa 1 cái mảng chứa nhiều phần tử
 
-                    formRules[input.name] = [validatorRules[rule]];
+                    formRules[input.name] = [valueValidatorRules];
                 }
             }
+
+            // Lắng nghe các sự kiến (blur, change, ...)
+            input.onblur = handleValidate;
+        }
+
+        function handleValidate(event) {
+            console.log(event);
         }
         console.log(formRules);
 
