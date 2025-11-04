ce5a478ed37d425f2133d985afbebbe1caa6ba67
Gia Long <gialongchuai@gmail.com>
2025-11-04 09:29:24 +0700

    js

---
+++ js/lesson_12/main.js
@@ -0,0 +1,12 @@
+
+
+// import / export
+
+import logger from './logger.js'
+import {
+    TYPE_ERROR,
+    TYPE_LOG,
+    TYPE_WARN
+} from './constants.js'
+
+logger('Message ...', TYPE_ERROR);
---
+++ js/lesson_12/test.js
@@ -0,0 +1,17 @@
+
+
+// test import
+
+import * as constants from './constants.js';
+
+console.log(constants);
+
+// Module {Symbol(Symbol.toStringTag): 'Module'}
+// TYPE_ERROR: "error"
+// TYPE_LOG: "log"
+// TYPE_WARN: "warn"
+// Symbol(Symbol.toStringTag): "Module"
+// get TYPE_ERROR: ƒ ()
+// set TYPE_ERROR: ƒ ()get TYPE_LOG: ƒ ()
+// set TYPE_LOG: ƒ ()get TYPE_WARN: ƒ ()
+// set TYPE_WARN: ƒ ()
